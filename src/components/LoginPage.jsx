73563089a885d342117142a72ab94d0c4b58986d
hawkeyemihawk07 <harshsb.43@gmail.com>
2026-02-25 22:16:53 +0530

    some small changes

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,17 @@
+function LoginPage() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="flex ">
+        <div className="">
+          <h1>GET STARTED</h1>
+          <img src="" alt="cross" />
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
